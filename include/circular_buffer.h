380f615cc51424a27411dca1e31ddf25b39854b9
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2016-05-24 19:43:36 +0100

    Fixed the circular buffer so that it works correctly

--- include/circular_buffer.h
+++ include/circular_buffer.h
@@ -6,8 +6,6 @@
 
 typedef struct _circular_buffer* circular_buffer;
 
-// initialize_circular_buffer
-//
 // This function initializes a circular buffer.
 circular_buffer circular_buffer_initialize(uint32_t size);
 
@@ -15,13 +13,9 @@ bool circular_buffer_add(circular_buffer buffer, uint32_t item);
 
 bool circular_buffer_get_next(circular_buffer buffer, uint32_t* item);
 
-bool circular_buffer_advance_if_next_equals(circular_buffer buffer,
-                                            uint32_t item);
-
-uint32_t circular_buffer_size(circular_buffer buffer);
+bool circular_buffer_advance_if_next_equals(
+    circular_buffer buffer, uint32_t item);
 
 uint32_t circular_buffer_get_n_buffer_overflows(circular_buffer buffer);
-
-void circular_buffer_print_buffer(circular_buffer buffer);
 
 #endif // _CIRCULAR_BUFFER_H_
