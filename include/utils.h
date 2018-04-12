1032795b7a7be01cc1078434661d2245317b0345
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2018-04-12 20:01:38 +0100

    Remove white space changes and use tabs for consistency

--- include/utils.h
+++ include/utils.h
@@ -37,7 +37,7 @@
 //! \param[in] x The 64-bit number
 //! \return The most significant 32-bits of x.
 static inline uint32_t __hi(
-    uint64_t x)
+	uint64_t x)
 {
     return (uint32_t) (x >> 32);
 }
@@ -47,7 +47,7 @@ static inline uint32_t __hi(
 //! \param[in] x The 64-bit number
 //! \return The least significant 32-bits of x.
 static inline uint32_t __lo(
-    uint64_t x)
+	uint64_t x)
 {
     return __hi(x << 32);
 }
@@ -57,12 +57,12 @@ static inline uint32_t __lo(
 //! \param[in] x The 64-bit number
 //! \return The rounded result.
 static inline uint64_t round64(
-    uint64_t x)
+	uint64_t x)
 {
     uint64_t r = (uint64_t) __hi(x);
 
     if (__lo(x) >= INT32_MAX) {
-    r++;
+	r++;
     }
     return r;
 }
@@ -74,8 +74,8 @@ static inline uint64_t round64(
 //! unsigned long fract.
 //! \return The rounded result.
 static inline uint64_t scale64(
-    uint64_t x,
-    uint32_t y)
+	uint64_t x,
+	uint32_t y)
 {
     uint64_t r = round64((uint64_t) __lo(x) * (uint64_t) y);
 
@@ -90,8 +90,8 @@ static inline uint64_t scale64(
 //! unsigned long fract.
 //! \return The rounded result.
 static inline uint32_t scale32(
-    uint32_t x,
-    uint32_t y)
+	uint32_t x,
+	uint32_t y)
 {
     return (uint32_t) round64((uint64_t) x * (uint64_t) y);
 }
@@ -100,35 +100,36 @@ static inline uint32_t scale32(
 // \param[in] v A 32-bit unsigned integer
 // \return The log of the next highest power of 2
 static inline uint32_t log_next_power_of_2(
-    uint32_t v)
+	uint32_t v)
 {
-    return 32 - __builtin_clz(v);
+	return 32 - __builtin_clz(v);
 }
 
 // \brief Returns the integer part of the log to base 2 of a number
 // \param[in] v A 32-bit unsigned integer
 // \return The log to the base 2
-static inline uint32_t ilog_2(uint32_t v)
+static inline uint32_t ilog_2(
+	uint32_t v)
 {
-    return 31 - __builtin_clz(v);
+	return 31 - __builtin_clz(v);
 }
 
 // \brief Returns the next highest power of 2 of a value
 // \param[in] v A 32-bit unsigned integer
 // \return The next highest power of 2
 static inline uint32_t next_power_of_2(
-    uint32_t v)
+	uint32_t v)
 {
-    return 1 << log_next_power_of_2(v);
+	return 1 << log_next_power_of_2(v);
 }
 
 // \brief Returns True if the value is a power of 2
 // \param[in] v A 32-bit unsigned integer
 // \return True if the value is a power of 2, False if not
 static inline bool is_power_of_2(
-    uint32_t v)
+	uint32_t v)
 {
-    return (v & (v - 1)) == 0;
+	return (v & (v - 1)) == 0;
 }
 
 #endif /*__UTILS_H__*/
