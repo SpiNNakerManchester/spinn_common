e7da5ad1aad5ebb898192cc23da99744e069ac1b
alan-stokes <alan.barry.stokes@gmail.com>
2020-04-28 11:51:33 +0100

    add warnings to the dodgy call.

--- include/bit_field.h
+++ include/bit_field.h
@@ -254,6 +254,9 @@ static inline size_t get_bit_field_size(
 }
 
 //! \brief Computes the number of set bits in a bit_field
+//! NOTE: THIS FUNCTION CAN ONLY BE TRUSTED IF set_bit_field HAS NOT BEEN
+//! CALLED ON THIS BITFIELD, as the count includes padding which can change
+//! value by the set_bit_field.
 //! \param[in] b The sequence of words representing a bit_field.
 //! \param[in] s The size of the bit_field.
 //! \return The function returns the number of bits set to 1.
