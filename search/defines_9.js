9602a45e790eecce9a8c56514732c7fd15294478
andrewgait <andrew.gait@manchester.ac.uk>
2022-11-11 12:33:38 +0000

    Deploying to gh-pages from  @ ea21671758f53490c86b1524269aa4267afbf6ee 🚀

--- search/defines_9.js
+++ search/defines_9.js
@@ -1,8 +1,8 @@
 var searchData=
 [
   ['r_0',['R',['../random_8c.html#a5c71a5e59a53413cd6c270266d63b031',1,'random.c']]],
-  ['random_1',['random',['../spin-print_8h.html#aaac7e8b42546e8a2fd07b71fd5b12ba0',1,'spin-print.h']]],
-  ['random_2',['RANDOM',['../round_8h.html#a69f1bba01cab6b4ca24a6f755e9a1980',1,'round.h']]],
+  ['random_1',['RANDOM',['../round_8h.html#a69f1bba01cab6b4ca24a6f755e9a1980',1,'round.h']]],
+  ['random_2',['random',['../spin-print_8h.html#aaac7e8b42546e8a2fd07b71fd5b12ba0',1,'spin-print.h']]],
   ['round_5fdown_5fs64_3',['ROUND_DOWN_S64',['../round_8h.html#ae31ffc9fae313f92af9ea8cd429c2160',1,'round.h']]],
   ['roundfx_4',['roundfx',['../stdfix-full-iso_8h.html#ace52ebdbaf21c275192d26dc67a6085b',1,'stdfix-full-iso.h']]],
   ['rounding_5fstochastic_5',['ROUNDING_STOCHASTIC',['../round_8h.html#ac7b933bc713c20406e9c9324c63fdce9',1,'round.h']]]
